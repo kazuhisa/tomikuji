507e0b33ce1cfc7ca9f10580d6f85e817e22a43b
Yamamoto Kazuhisa <ak.hisashi@gmail.com>
2012-11-26 20:58:02 +0900

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tomikuji::Application.config.secret_token = '4eac56bdd75f43b8ab8a05989943639de3f157f1bb2f608afdd8a91e8d43cba22de8b133887eca8d851c2885c75257c09eacde2b8ee5bb3cd727e691834bdd77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tomikuji::Application.config.session_store :cookie_store, key: '_tomikuji_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tomikuji::Application.config.session_store :active_record_store
